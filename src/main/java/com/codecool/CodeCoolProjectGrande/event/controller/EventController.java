cfdb2483c34f05b82b0112c3b1c608f829fbf070
Matt <mattwasilewski96@gmail.com>
2023-02-24 15:06:47 +0100

    removed unused imports/thymeleaf templates and comments

--- src/main/java/com/codecool/CodeCoolProjectGrande/event/controller/EventController.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/event/controller/EventController.java
@@ -3,9 +3,6 @@ package com.codecool.CodeCoolProjectGrande.event.controller;
 import com.codecool.CodeCoolProjectGrande.event.Event;
 import com.codecool.CodeCoolProjectGrande.event.EventType;
 import com.codecool.CodeCoolProjectGrande.event.service.EventServiceImpl;
-import org.hibernate.event.spi.SaveOrUpdateEvent;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.*;
 
 
